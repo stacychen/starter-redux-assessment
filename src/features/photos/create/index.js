bc3640ef096a98817931736eb677dc89fcb0caff
Stacy Chen <schen@indeed.com>
2023-11-24 11:54:59 +0800

    Add redux assessment

--- src/features/photos/create/index.js
+++ src/features/photos/create/index.js
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 // Task 2: Import the `useDispatch()` method from the appropriate package
-// Task 3: Import the `addPhoto()` action creator from the photos slice
 
-import './create.css';
+import { useDispatch } from "react-redux";
+// Task 3: Import the `addPhoto()` action creator from the photos slice
+import { addPhoto } from "../photos.slice";
+
+import "./create.css";
 
 export default function CreatePhoto() {
-  const [formData, setFormData] = useState({ imageUrl: '', caption: '' });
+  const [formData, setFormData] = useState({ imageUrl: "", caption: "" });
   // Task 4: Store a reference to the Redux store's dispatch method in a variable called `dispatch`
+  const dispatch = useDispatch();
 
   function handleChange({ target: { name, value } }) {
     setFormData({
@@ -19,7 +23,8 @@ export default function CreatePhoto() {
   function handleSubmit(event) {
     event.preventDefault();
     // Task 5: Dispatch the `addPhoto()` action creator, passing in the form data
-    setFormData({ imageUrl: '', caption: '' });
+    dispatch(addPhoto(formData));
+    setFormData({ imageUrl: "", caption: "" });
   }
 
   return (
